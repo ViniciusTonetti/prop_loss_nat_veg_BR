b647f874ec6ebcc0fc00c54757076df5662293ca
ViniciusTonetti <vrtonetti@gmail.com>
2024-05-29 20:40:36 -0300

    Starting code

---
+++ analysis.R
@@ -0,0 +1,12 @@
+# Code to create curves of proportional loss of native vegetation in Brazil through presidential terms
+# Vinicius Tonetti  - vrtonetti@gmail.com
+# ------------------------------------------------------------------------------
+
+# Packages ---------------------------------------------------------------------
+
+library(tidyverse)
+library(xlsx)
+
+install.packages("xlsx")
+
+# Loading data -----------------------------------------------------------------
